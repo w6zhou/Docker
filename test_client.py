b1d4c6e176c2ee85d97d0f06a516d004321dc356
Wenqi Zhou <zhou.wenqi@hotmail.com>
2018-04-13 09:40:51 +0800

    update client

--- test_client.py
+++ test_client.py
@@ -4,7 +4,7 @@ import helloword_pb2, helloword_pb2_grpc
 
 
 def box_unlock():
-    channel = grpc.insecure_channel("%s:%d" % ('10.233.65.95', '54321'))
+    channel = grpc.insecure_channel("%s:%s" % ('10.233.65.95', '54321'))
     stub = helloword_pb2_grpc.GreeterStub(channel)
     kwargs = {'name': 'wenqi', 'name2': 'wenqi2'}
     req = helloword_pb2.HelloRequest(**kwargs)
